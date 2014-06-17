d4be545d67ca4c45b859a91ba36fda442e7a9dc6
Alan Shreve <alan@inconshreveable.com>
2014-06-16 22:32:42 -0700

    don't panic on non-windows platforms. add doc to non-windows file so it shows up in godoc

--- trap_others.go
+++ trap_others.go
@@ -2,6 +2,14 @@
 
 package mousetrap
 
-func StartedByExplorer() (bool) {
-    panic("Unimplemented")
+// StartedByExplorer returns true if the program was invoked by the user
+// double-clicking on the executable from explorer.exe
+//
+// It is conservative and returns false if any of the internal calls fail.
+// It does not guarantee that the program was run from a terminal. It only can tell you
+// whether it was launched from explorer.exe
+//
+// On non-Windows platforms, it always returns false.
+func StartedByExplorer() bool {
+	return false
 }
