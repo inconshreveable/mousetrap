b4f7ad8fb5ba1cdcd10a1799267e5327ea13eb7a
Alan Shreve <alan@inconshreveable.com>
2014-09-09 13:25:47 -0700

    add 1.4 implementation

--- trap_windows.go
+++ trap_windows.go
@@ -1,78 +1,79 @@
 // +build windows
+// +build !go1.4
 
 package mousetrap
 
 import (
-    "os"
-    "fmt"
-    "syscall"
-    "unsafe"
+	"fmt"
+	"os"
+	"syscall"
+	"unsafe"
 )
 
 const (
-    // defined by the Win32 API
-    th32cs_snapprocess uintptr = 0x2
+	// defined by the Win32 API
+	th32cs_snapprocess uintptr = 0x2
 )
 
 var (
-    kernel = syscall.MustLoadDLL("kernel32.dll")
-    CreateToolhelp32Snapshot = kernel.MustFindProc("CreateToolhelp32Snapshot")
-    Process32First = kernel.MustFindProc("Process32FirstW")
-    Process32Next = kernel.MustFindProc("Process32NextW")
+	kernel                   = syscall.MustLoadDLL("kernel32.dll")
+	CreateToolhelp32Snapshot = kernel.MustFindProc("CreateToolhelp32Snapshot")
+	Process32First           = kernel.MustFindProc("Process32FirstW")
+	Process32Next            = kernel.MustFindProc("Process32NextW")
 )
 
 // ProcessEntry32 structure defined by the Win32 API
 type processEntry32 struct {
-    dwSize uint32
-    cntUsage uint32
-    th32ProcessID uint32
-    th32DefaultHeapID int
-    th32ModuleID uint32
-    cntThreads uint32
-    th32ParentProcessID uint32
-    pcPriClassBase int32
-    dwFlags uint32
-    szExeFile [syscall.MAX_PATH]uint16
+	dwSize              uint32
+	cntUsage            uint32
+	th32ProcessID       uint32
+	th32DefaultHeapID   int
+	th32ModuleID        uint32
+	cntThreads          uint32
+	th32ParentProcessID uint32
+	pcPriClassBase      int32
+	dwFlags             uint32
+	szExeFile           [syscall.MAX_PATH]uint16
 }
 
 func getProcessEntry(pid int) (pe *processEntry32, err error) {
-    snapshot, _, e1 := CreateToolhelp32Snapshot.Call(th32cs_snapprocess, uintptr(0));
-    if (snapshot == uintptr(syscall.InvalidHandle)) {
-        err = fmt.Errorf("CreateToolhelp32Snapshot: %v", e1)
-        return
-    }
-    defer syscall.CloseHandle(syscall.Handle(snapshot))
+	snapshot, _, e1 := CreateToolhelp32Snapshot.Call(th32cs_snapprocess, uintptr(0))
+	if snapshot == uintptr(syscall.InvalidHandle) {
+		err = fmt.Errorf("CreateToolhelp32Snapshot: %v", e1)
+		return
+	}
+	defer syscall.CloseHandle(syscall.Handle(snapshot))
 
-    var processEntry processEntry32
-    processEntry.dwSize = uint32(unsafe.Sizeof(processEntry))
-    ok, _ , e1 := Process32First.Call(snapshot, uintptr(unsafe.Pointer(&processEntry)))
-    if ok == 0 {
-        err = fmt.Errorf("Process32First: %v", e1)
-        return
-    }
+	var processEntry processEntry32
+	processEntry.dwSize = uint32(unsafe.Sizeof(processEntry))
+	ok, _, e1 := Process32First.Call(snapshot, uintptr(unsafe.Pointer(&processEntry)))
+	if ok == 0 {
+		err = fmt.Errorf("Process32First: %v", e1)
+		return
+	}
 
-    for {
-        if processEntry.th32ProcessID == uint32(pid) {
-            pe = &processEntry
-            return
-        }
+	for {
+		if processEntry.th32ProcessID == uint32(pid) {
+			pe = &processEntry
+			return
+		}
 
-        ok, _, e1 = Process32Next.Call(snapshot, uintptr(unsafe.Pointer(&processEntry)))
-        if ok == 0 {
-            err = fmt.Errorf("Process32Next: %v", e1)
-            return
-        }
-    }
+		ok, _, e1 = Process32Next.Call(snapshot, uintptr(unsafe.Pointer(&processEntry)))
+		if ok == 0 {
+			err = fmt.Errorf("Process32Next: %v", e1)
+			return
+		}
+	}
 }
 
 func getppid() (pid int, err error) {
-    pe, err := getProcessEntry(os.Getpid())
-    if err != nil {
-        return
-    }
+	pe, err := getProcessEntry(os.Getpid())
+	if err != nil {
+		return
+	}
 
-    pid = int(pe.th32ParentProcessID)
-    return
+	pid = int(pe.th32ParentProcessID)
+	return
 }
 
 // StartedByExplorer returns true if the program was invoked by the user double-clicking
@@ -81,17 +82,17 @@ func getppid() (pid int, err error) {
 // It is conservative and returns false if any of the internal calls fail.
 // It does not guarantee that the program was run from a terminal. It only can tell you
 // whether it was launched from explorer.exe
-func StartedByExplorer() (bool) {
-    ppid, err := getppid()
-    if err != nil {
-        return false
-    }
+func StartedByExplorer() bool {
+	ppid, err := getppid()
+	if err != nil {
+		return false
+	}
 
-    pe, err := getProcessEntry(ppid)
-    if err != nil {
-        return false
-    }
+	pe, err := getProcessEntry(ppid)
+	if err != nil {
+		return false
+	}
 
-    name := syscall.UTF16ToString(pe.szExeFile[:])
-    return name == "explorer.exe"
+	name := syscall.UTF16ToString(pe.szExeFile[:])
+	return name == "explorer.exe"
 }
